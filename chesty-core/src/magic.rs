d2535903c05a475e271a71fb438987104e9fea73
flauntingspade4 <elliotwhybrow@gmail.com>
2022-02-06 00:05:27 +0000

    Not yet working

--- chesty-core/src/magic.rs
+++ chesty-core/src/magic.rs
@@ -134,7 +134,7 @@ pub const KNIGHT_ATTACKS: [u64; 64] = [
     0x20400000000000,
 ];
 
-const SQUARE_BB: [u64; 65] = [
+pub const SQUARE_BB: [u64; 65] = [
     0x1,
     0x2,
     0x4,
@@ -223,6 +223,143 @@ const MASK_RANK: [u64; 8] = [
     0xff000000000000,
     0xff00000000000000,
 ];
+
+const ROOK_MAGICS: [u64; 64] = [
+    0xa8002c000108020,
+    0x6c00049b0002001,
+    0x100200010090040,
+    0x2480041000800801,
+    0x280028004000800,
+    0x900410008040022,
+    0x280020001001080,
+    0x2880002041000080,
+    0xa000800080400034,
+    0x4808020004000,
+    0x2290802004801000,
+    0x411000d00100020,
+    0x402800800040080,
+    0xb000401004208,
+    0x2409000100040200,
+    0x1002100004082,
+    0x22878001e24000,
+    0x1090810021004010,
+    0x801030040200012,
+    0x500808008001000,
+    0xa08018014000880,
+    0x8000808004000200,
+    0x201008080010200,
+    0x801020000441091,
+    0x800080204005,
+    0x1040200040100048,
+    0x120200402082,
+    0xd14880480100080,
+    0x12040280080080,
+    0x100040080020080,
+    0x9020010080800200,
+    0x813241200148449,
+    0x491604001800080,
+    0x100401000402001,
+    0x4820010021001040,
+    0x400402202000812,
+    0x209009005000802,
+    0x810800601800400,
+    0x4301083214000150,
+    0x204026458e001401,
+    0x40204000808000,
+    0x8001008040010020,
+    0x8410820820420010,
+    0x1003001000090020,
+    0x804040008008080,
+    0x12000810020004,
+    0x1000100200040208,
+    0x430000a044020001,
+    0x280009023410300,
+    0xe0100040002240,
+    0x200100401700,
+    0x2244100408008080,
+    0x8000400801980,
+    0x2000810040200,
+    0x8010100228810400,
+    0x2000009044210200,
+    0x4080008040102101,
+    0x40002080411d01,
+    0x2005524060000901,
+    0x502001008400422,
+    0x489a000810200402,
+    0x1004400080a13,
+    0x4000011008020084,
+    0x26002114058042,
+];
+
+const BISHOP_MAGICS: [u64; 64] = [
+    0x89a1121896040240,
+    0x2004844802002010,
+    0x2068080051921000,
+    0x62880a0220200808,
+    0x4042004000000,
+    0x100822020200011,
+    0xc00444222012000a,
+    0x28808801216001,
+    0x400492088408100,
+    0x201c401040c0084,
+    0x840800910a0010,
+    0x82080240060,
+    0x2000840504006000,
+    0x30010c4108405004,
+    0x1008005410080802,
+    0x8144042209100900,
+    0x208081020014400,
+    0x4800201208ca00,
+    0xf18140408012008,
+    0x1004002802102001,
+    0x841000820080811,
+    0x40200200a42008,
+    0x800054042000,
+    0x88010400410c9000,
+    0x520040470104290,
+    0x1004040051500081,
+    0x2002081833080021,
+    0x400c00c010142,
+    0x941408200c002000,
+    0x658810000806011,
+    0x188071040440a00,
+    0x4800404002011c00,
+    0x104442040404200,
+    0x511080202091021,
+    0x4022401120400,
+    0x80c0040400080120,
+    0x8040010040820802,
+    0x480810700020090,
+    0x102008e00040242,
+    0x809005202050100,
+    0x8002024220104080,
+    0x431008804142000,
+    0x19001802081400,
+    0x200014208040080,
+    0x3308082008200100,
+    0x41010500040c020,
+    0x4012020c04210308,
+    0x208220a202004080,
+    0x111040120082000,
+    0x6803040141280a00,
+    0x2101004202410000,
+    0x8200000041108022,
+    0x21082088000,
+    0x2410204010040,
+    0x40100400809000,
+    0x822088220820214,
+    0x40808090012004,
+    0x910224040218c9,
+    0x402814422015008,
+    0x90014004842410,
+    0x1000042304105,
+    0x10008830412a00,
+    0x2520081090008908,
+    0x40102000a0a60140,
+];
+
+const ROOK_MASKS: [u64; 64] = init_rook_masks();
+const BISHOP_MASKS: [u64; 64] = init_bishop_masks();
 
 const ROOK_TABLE: [SMagic; 64] = [
     SMagic::new(0x101010101017e, 0x80001020400080, 52),
@@ -358,7 +495,7 @@ const BISHOP_TABLE: [SMagic; 64] = [
     SMagic::new(0x40201008040200, 0x2020202020200, 58),
 ];
 
-const MASK_DIAGONAL: [u64; 15] = [
+/*const MASK_DIAGONAL: [u64; 15] = [
     0x80,
     0x8040,
     0x804020,
@@ -393,12 +530,12 @@ const MASK_ANTI_DIAGONAL: [u64; 15] = [
     0x2040800000000000,
     0x4080000000000000,
     0x8000000000000000,
-];
+];*/
 
 const ROOK_ATTACKS: [[u64; 64]; 4096] = init_rook_attacks();
-const BISHOP_ATTACKS: [[u64; 64]; 512] = init_bishop_attacks();
+const BISHOP_ATTACKS: [[u64; 64]; 1024] = init_bishop_attacks();
 
-const fn diagonal_of(position: Position) -> usize {
+/*const fn diagonal_of(position: Position) -> usize {
     7 + position.y() as usize - position.x() as usize
 }
 const fn anti_diagonal_of(position: Position) -> usize {
@@ -406,9 +543,11 @@ const fn anti_diagonal_of(position: Position) -> usize {
 }
 
 const fn sliding_attacks(position: Position, subset: u64, mask: u64) -> u64 {
-    (((mask & subset) - SQUARE_BB[position.index() as usize] * 2)
-        ^ ((mask & subset).reverse_bits()
-            - SQUARE_BB[position.index() as usize].reverse_bits() * 2))
+    (((mask & subset).saturating_sub(SQUARE_BB[position.index() as usize] * 2))
+        ^ ((mask & subset)
+            .reverse_bits()
+            .saturating_sub(SQUARE_BB[position.index() as usize].reverse_bits())
+            * 2))
         .reverse_bits()
         & mask
 }
@@ -421,19 +560,24 @@ const fn get_rook_attacks_for_init(position: Position, subset: u64) -> u64 {
 const fn init_rook_attacks() -> [[u64; 64]; 4096] {
     let mut attacks = [[0; 64]; 4096];
 
-    let mut subset = 1;
     let mut index;
 
     let mut square: usize = 0;
 
     while square < 64 {
-        while subset != 0 {
+        let mut subset = 0;
+
+        loop {
             index = subset;
             index *= ROOK_TABLE[square].magic;
             index >>= ROOK_TABLE[square].shift;
-            attacks[index as usize][square] =
+            attacks[square][index as usize] =
                 get_rook_attacks_for_init(Position::from_u8(square as u8), subset);
-            subset = (subset - ROOK_TABLE[square].mask) & ROOK_TABLE[square].mask;
+            subset = (subset.saturating_sub(ROOK_TABLE[square].mask)) & ROOK_TABLE[square].mask;
+
+            if subset == 0 {
+                break;
+            }
         }
 
         square += 1;
@@ -454,19 +598,233 @@ const fn get_bishop_attacks_for_init(position: Position, subset: u64) -> u64 {
 const fn init_bishop_attacks() -> [[u64; 64]; 512] {
     let mut attacks = [[0; 64]; 512];
 
-    let mut subset = 1;
     let mut index;
 
     let mut square: usize = 0;
 
     while square < 64 {
-        while subset != 0 {
+        let mut subset = 0;
+
+        loop {
             index = subset;
             index *= BISHOP_TABLE[square].magic;
             index >>= BISHOP_TABLE[square].shift;
-            attacks[index as usize][square] =
+            attacks[square][index as usize] =
                 get_bishop_attacks_for_init(Position::from_u8(square as u8), subset);
-            subset = (subset - BISHOP_TABLE[square].mask) & BISHOP_TABLE[square].mask;
+            subset = (subset.saturating_sub(BISHOP_TABLE[square].mask)) & BISHOP_TABLE[square].mask;
+
+            if subset == 0 {
+                break;
+            }
+        }
+
+        square += 1;
+    }
+
+    attacks
+}*/
+
+const ROOK_INDEX_BITS: [u64; 64] = [
+    12, 11, 11, 11, 11, 11, 11, 12, 11, 10, 10, 10, 10, 10, 10, 11, 11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11, 11, 10, 10, 10, 10, 10, 10, 11, 11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11, 12, 11, 11, 11, 11, 11, 11, 12,
+];
+
+const BISHOP_INDEX_BITS: [u64; 64] = [
+    6, 5, 5, 5, 5, 5, 5, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 5, 5, 5, 5, 7, 9, 9, 7, 5, 5,
+    5, 5, 7, 9, 9, 7, 5, 5, 5, 5, 7, 7, 7, 7, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 5, 5, 5, 5, 5, 5, 6,
+];
+
+const LSB_64_TABLE: [u8; 154] = [
+    22, 0, 0, 0, 30, 0, 0, 38, 18, 0, 16, 15, 17, 0, 46, 9, 19, 8, 7, 10, 0, 63, 1, 56, 55, 57, 2,
+    11, 0, 58, 0, 0, 20, 0, 3, 0, 0, 59, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 4, 0, 0, 60, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 0, 29, 0, 0, 37, 0, 0, 0, 13, 0, 0, 45, 0, 0, 0, 5, 0, 0, 61, 0,
+    0, 0, 53, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 28, 0, 0, 36, 0, 0, 0, 0, 0, 0, 44, 0, 0, 0, 0, 0, 27,
+    0, 0, 35, 0, 52, 0, 0, 26, 0, 43, 34, 25, 23, 24, 33, 31, 32, 42, 39, 40, 51, 41, 14, 0, 49,
+    47, 48, 0, 50, 6, 0, 0, 62, 0, 0, 0, 54,
+];
+
+const fn pop_lsb(mask: &mut u64) -> u64 {
+    let index = bitscan_forward(*mask);
+
+    *mask ^= 1 << index;
+
+    index as u64
+}
+
+const fn bitscan_forward(mut board: u64) -> u64 {
+    let mut t32;
+    board ^= board - 1;
+    t32 = board ^ (board >> 32);
+    t32 ^= 0x01C5FC81;
+    t32 += t32 >> 16;
+    t32 -= (t32 >> 8) + 51;
+    LSB_64_TABLE[(t32 & 255) as usize] as u64
+}
+
+const fn bitscan_backward(board: u64) -> u64 {
+    64 - bitscan_forward(board.reverse_bits())
+}
+
+const fn get_rook_attacks_slow(square: u64, blockers: u64) -> u64 {
+    let mut attacks = 0;
+    let square = square as usize;
+
+    // North
+    attacks |= RAYS[square][Dir::North as usize];
+    if RAYS[square][Dir::North as usize] & blockers != 0 {
+        attacks &= !RAYS[bitscan_forward(RAYS[square][Dir::North as usize] & blockers) as usize]
+            [Dir::North as usize];
+    }
+
+    // South
+    attacks |= RAYS[square][Dir::South as usize];
+    if RAYS[square][Dir::South as usize] & blockers != 0 {
+        attacks &= !RAYS[bitscan_backward(RAYS[square][Dir::South as usize] & blockers) as usize]
+            [Dir::South as usize];
+    }
+
+    // East
+    attacks |= RAYS[square][Dir::East as usize];
+    if RAYS[square][Dir::East as usize] & blockers != 0 {
+        attacks &= !RAYS[bitscan_forward(RAYS[square][Dir::East as usize] & blockers) as usize]
+            [Dir::East as usize];
+    }
+
+    // West
+    attacks |= RAYS[square][Dir::West as usize];
+    if RAYS[square][Dir::West as usize] & blockers != 0 {
+        attacks &= !RAYS[bitscan_backward(RAYS[square][Dir::West as usize] & blockers) as usize]
+            [Dir::West as usize];
+    }
+
+    attacks
+}
+
+const fn get_bishop_attacks_slow(square: u64, blockers: u64) -> u64 {
+    let mut attacks = 0;
+    let square = square as usize;
+
+    // North
+    attacks |= RAYS[Dir::NorthWest as usize][square];
+    if RAYS[Dir::NorthWest as usize][square] & blockers != 0 {
+        attacks &= !RAYS[Dir::NorthWest as usize]
+            [bitscan_forward(RAYS[Dir::NorthWest as usize][square] & blockers) as usize];
+    }
+
+    // South
+    attacks |= RAYS[Dir::NorthEast as usize][square];
+    if RAYS[Dir::NorthEast as usize][square] & blockers != 0 {
+        attacks &= !RAYS[Dir::NorthEast as usize]
+            [bitscan_backward(RAYS[Dir::NorthEast as usize][square] & blockers) as usize];
+    }
+
+    // East
+    attacks |= RAYS[Dir::SouthEast as usize][square];
+    if RAYS[Dir::SouthEast as usize][square] & blockers != 0 {
+        attacks &= !RAYS[Dir::SouthEast as usize]
+            [bitscan_forward(RAYS[Dir::SouthEast as usize][square] & blockers) as usize];
+    }
+
+    // West
+    attacks |= RAYS[Dir::SouthWest as usize][square];
+    if RAYS[Dir::SouthWest as usize][square] & blockers != 0 {
+        attacks &= !RAYS[Dir::SouthWest as usize]
+            [bitscan_backward(RAYS[Dir::SouthWest as usize][square] & blockers) as usize];
+    }
+
+    attacks
+}
+
+const fn get_blockers_from_index(index: u64, mut mask: u64) -> u64 {
+    let mut blockers = 0;
+    let bits = mask.count_ones();
+
+    let mut i = 0;
+
+    while i < bits {
+        let bit_pos = pop_lsb(&mut mask);
+        if (index & (1 << i)) != 0 {
+            blockers |= 1 << bit_pos;
+        }
+
+        i += 1;
+    }
+
+    blockers
+}
+
+const fn init_rook_masks() -> [u64; 64] {
+    let mut masks = [0; 64];
+
+    let mut square = 0;
+
+    while square < 64 {
+        masks[square] = (RAYS[square][Dir::North as usize] & !MASK_RANK[7])
+            | (RAYS[square][Dir::South as usize] & !MASK_RANK[0])
+            | (RAYS[square][Dir::East as usize] & !MASK_FILE[7])
+            | (RAYS[square][Dir::West as usize] & !MASK_FILE[0]);
+        square += 1;
+    }
+
+    masks
+}
+
+const fn init_bishop_masks() -> [u64; 64] {
+    let mut masks = [0; 64];
+    let edge_squares = MASK_FILE[0] | MASK_FILE[7] | MASK_RANK[0] | MASK_RANK[7];
+
+    let mut square = 0;
+
+    while square < 64 {
+        masks[square] = (RAYS[square][Dir::NorthEast as usize]
+            | RAYS[square][Dir::NorthWest as usize]
+            | RAYS[square][Dir::SouthWest as usize]
+            | RAYS[square][Dir::SouthEast as usize])
+            & !edge_squares;
+
+        square += 1;
+    }
+
+    masks
+}
+
+const fn init_rook_attacks() -> [[u64; 64]; 4096] {
+    let mut attacks = [[0; 64]; 4096];
+
+    let mut square = 0;
+
+    while square < 64 {
+        let mut blocker_index = 0;
+
+        while blocker_index < (1 << ROOK_INDEX_BITS[square]) {
+            let blockers = get_blockers_from_index(blocker_index, ROOK_MASKS[square]);
+            attacks[((blockers * ROOK_MAGICS[square]) >> (64 - ROOK_INDEX_BITS[square]))
+                as usize][square] = get_rook_attacks_slow(square as u64, blockers);
+
+            blocker_index += 1;
+        }
+
+        square += 1;
+    }
+
+    attacks
+}
+
+const fn init_bishop_attacks() -> [[u64; 64]; 1024] {
+    let mut attacks = [[0; 64]; 1024];
+
+    let mut square = 0;
+
+    while square < 64 {
+        let mut blocker_index = 0;
+
+        while blocker_index < (1 << BISHOP_INDEX_BITS[square]) {
+            let blockers = get_blockers_from_index(blocker_index, BISHOP_MASKS[square]);
+            attacks[((blockers * BISHOP_MAGICS[square]) >> (64 - BISHOP_INDEX_BITS[square]))
+                as usize][square] = get_bishop_attacks_slow(square as u64, blockers);
+
+            blocker_index += 1;
         }
 
         square += 1;
@@ -500,4 +858,92 @@ impl SMagic {
     pub const fn new(mask: u64, magic: u64, shift: u64) -> Self {
         Self { mask, magic, shift }
     }
+}
+
+#[repr(u8)]
+enum Dir {
+    North,
+    South,
+    East,
+    West,
+    NorthEast,
+    NorthWest,
+    SouthEast,
+    SouthWest,
+}
+
+const RAYS: [[u64; 8]; 64] = init_rays();
+
+const fn init_rays() -> [[u64; 8]; 64] {
+    let mut rays = [[0; 8]; 64];
+
+    let mut square = 0;
+
+    while square < 64 {
+        // North
+        rays[square][Dir::North as usize] = 0x0101010101010100 << square;
+
+        // South
+        rays[square][Dir::South as usize] = 0x0080808080808080 >> (63 - square);
+
+        // East
+        rays[square][Dir::East as usize] = 2 * ((1 << (square | 7)) - (1 << square));
+
+        // West
+        rays[square][Dir::West as usize] = (1 << square) - (1 << (square & 56));
+
+        // North West
+        rays[square][Dir::NorthWest as usize] =
+            north_west(0x102040810204000, 7 - col(square)) << (row(square) * 8);
+
+        // North East
+        rays[square][Dir::NorthEast as usize] =
+            north_east(0x8040201008040200, col(square)) << (row(square) * 8);
+
+        // South West
+        rays[square][Dir::SouthWest as usize] =
+            north_west(0x40201008040201, 7 - col(square)) >> ((7 - row(square)) * 8);
+
+        // South East
+        rays[square][Dir::SouthEast as usize] =
+            north_east(0x2040810204080, col(square)) >> ((7 - row(square)) * 8);
+
+        square += 1;
+    }
+
+    rays
+}
+
+const fn north_west(board: u64, n: u64) -> u64 {
+    let mut new_board = board;
+    let mut i = 0;
+
+    while i < n {
+        new_board = (new_board >> 1) & (!MASK_FILE[7]);
+
+        i += 1;
+    }
+
+    new_board
+}
+
+const fn north_east(board: u64, n: u64) -> u64 {
+    let mut new_board = board;
+    let mut i = 0;
+
+    while i < n {
+        new_board = (new_board >> 1) & (!MASK_FILE[0]);
+
+        i += 1;
+    }
+
+    new_board
+}
+
+const fn row(square: usize) -> u64 {
+    (square >> 3) as u64
+}
+
+const fn col(square: usize) -> u64 {
+    (square % 8) as u64
 }
