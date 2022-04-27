144d1fcdf5ae4ee9ae9b6414e2e3309293a208b5
flauntingspade4 <elliotwhybrow@gmail.com>
2022-04-27 22:22:57 +0100

    ASd

--- chesty-core/src/move_ordering.rs
+++ chesty-core/src/move_ordering.rs
@@ -28,9 +28,11 @@ pub fn move_ordering(
                 killer_table[ply as usize].contains_move(possible_move.0, possible_move.1),
             ) * 250;
         } else {
-            possible_move.2 += ((take_value as u16).saturating_mul(TOOK_PIECE_MULTIPLIER))
-                - ((board[possible_move.0].piece_value() as u16)
-                    .saturating_mul(TAKING_PIECE_MULTIPLIER));
+            possible_move.2 = possible_move.2.saturating_add(
+                ((take_value as u16).saturating_mul(TOOK_PIECE_MULTIPLIER))
+                    - ((board[possible_move.0].piece_value() as u16)
+                        .saturating_mul(TAKING_PIECE_MULTIPLIER)),
+            );
         }
     }
 
@@ -41,9 +43,11 @@ pub fn quiescence_move_ordering(board: &Board, moves: &mut [(Position, Position,
     for possible_move in moves.iter_mut() {
         let take_value = board[possible_move.1].piece_value();
         if take_value != 0 {
-            possible_move.2 += ((take_value as u16).saturating_mul(TOOK_PIECE_MULTIPLIER))
-                - ((board[possible_move.0].piece_value() as u16)
-                    .saturating_mul(TAKING_PIECE_MULTIPLIER));
+            possible_move.2 = possible_move.2.saturating_add(
+                ((take_value as u16).saturating_mul(TOOK_PIECE_MULTIPLIER))
+                    - ((board[possible_move.0].piece_value() as u16)
+                        .saturating_mul(TAKING_PIECE_MULTIPLIER)),
+            );
         }
     }
 
